50876b28c804eedfe5e508326218cedb6947d90a
JeetGaikwad <jeetgaikwad95@gmail.com>
2025-05-24 11:22:52 +0530

    React-Quiz-App

---
+++ Quiz-App/src/components/Quiz.jsx
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions.js";
+import Question from "./Question.jsx";
+import ShowResult from "./ShowResult.jsx";
+
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
+  const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswer((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+      <ShowResult userAnswer={userAnswer} />
+    );
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
